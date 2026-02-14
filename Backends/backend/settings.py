384f4eff6217ebb7d1a3afda3158eb1c8c714a76
stefan-bouognong <stefanbouognong@gmail.com>
2026-02-14 16:45:41 +0100

    preparation du back pour la production

--- Backends/backend/settings.py
+++ Backends/backend/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 # ======================
 # APPLICATIONS
